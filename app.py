1469d1e5efa0118ad38fd033edb822fd15631bc7
Gene Cheng <cgm7487@users.noreply.github.com>
2025-06-09 11:50:36 +0800

    Update web page title

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ INDEX_HTML = """
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>LeetCode Random Selector</title>
+    <title>Code Trainer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   </head>
@@ -93,7 +93,7 @@ SOLVE_HTML = """
 <html lang=\"en\">
   <head>
     <meta charset=\"utf-8\">
-    <title>Solve Problem</title>
+    <title>Code Trainer</title>
     <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\">
   </head>
   <body class=\"bg-light\">
